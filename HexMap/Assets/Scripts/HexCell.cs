21f8b35d34d2b58774f2ee673da3c469f2329207
ChuanChinLai <chuanchinlai@gmail.com>
2019-04-26 21:11:59 -0700

    working on blending color

--- HexMap/Assets/Scripts/HexCell.cs
+++ HexMap/Assets/Scripts/HexCell.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HexCell : MonoBehaviour
 {
     public HexCoordinates coordinates;
 
     public Color color;
+
+    [SerializeField]
+    HexCell[] neighbors;
 
     // Use this for initialization
     void Start ()
@@ -19,4 +23,16 @@ public class HexCell : MonoBehaviour
     {
 		
 	}
+
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
+
 }
